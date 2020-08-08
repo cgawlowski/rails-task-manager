4350f03281ed74f7f693bb21b8c96021afbc5381
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-08-08 15:19:35 -0400

    Step 6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,9 +18,12 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(params[:task])
-    redirect_to task_path(@task)
+    redirect_to tasks_path
   end
-  def destroy  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
